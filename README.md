e8e92a7adae860cf0094b6a12f0df55f30745cdf
cawfree <hello@cawfree.com>
2019-10-23 11:12:22 +0100

    🚀 Published to GitHub!

--- README.md
+++ README.md
@@ -1,2 +1,92 @@
-# react-native-rotary
-A &lt;Rotary /> component for React Native.
+# 🕰️  react-native-rotary
+A `<Rotary />` component for React Native, compatible with Android, iOS and Web, powered by [react-native-animated-math](https://github.com/rastapasta/react-native-animated-math).
+
+This project supports [Semantic Versioning](https://docs.npmjs.com/about-semantic-versioning).
+
+## 🚀 Getting Started
+
+Using [`npm`]():
+
+```shell
+npm install --save react-native-rotary
+```
+
+Using [`yarn`]():
+
+```shell
+yarn add react-native-rotary
+```
+
+### 🔥 Features
+  - Dynamic paginated rotation control via the `index` prop.
+  - Smoothly animated infinite content loops, without the need for duplicate entries, scroll limits or jitter.
+
+## ✍️ Example
+
+This component emulates the configuration of a [`<FlatList />`](https://facebook.github.io/react-native/docs/flatlist).
+
+```javascript
+import React, { useState } from 'react';
+import { View, StyleSheet, Image } from 'react-native';
+import Rotary from 'react-native-rotary';
+
+export default () => {
+  // You can define an initial index offset, and render arbitrary child elements.
+  const [ index, setIndex ] = useState(1);
+  return (
+    <View
+      style={[
+        StyleSheet.absoluteFill,
+        {
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
+      ]}
+    >
+      <Rotary
+        index={index}
+        onIndexChanged={setIndex}
+        radius={100}
+        data={data}
+        renderItem={({ item: uri, index }) => (
+          <Image
+            style={{
+              width: 50,
+              height: 50,
+            }}
+            source={{ uri }}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+const data = [
+  'https://mondrian.mashable.com/uploads%252Fcard%252Fimage%252F929108%252F46c9313d-32d0-4da8-8d41-f5e50936a926.png%252Ffull-fit-in__950x534.png?signature=_R0yeIihD3oDvF1bulncd718gR0=&source=https%3A%2F%2Fblueprint-api-production.s3.amazonaws.com',
+  'https://cdn-01.independent.ie/incoming/article34131003.ece/bcec2/AUTOCROP/w620/Hugging%20Face.png',
+  'https://i2.wp.com/www.emojifoundation.com/wp-content/uploads/2017/07/Thinking_Face_Emoji.png',
+  'https://cdn.shopify.com/s/files/1/1061/1924/products/Unamused_Face_Emoji_761d8bf8-c78c-45b1-80b1-a86a80d2452d_grande.png?v=1480481058',
+  'https://cdn.shopify.com/s/files/1/1061/1924/products/Cold_Sweat_Emoji_grande.png?v=1480481051',
+  'https://thumbor.forbes.com/thumbor/960x0/https%3A%2F%2Fblogs-images.forbes.com%2Fdavidphelan%2Ffiles%2F2017%2F07%2Femoji_update_2017_10.jpg',
+  'https://cdn.shopify.com/s/files/1/1061/1924/products/Face_with_Cold_Sweat_Emoji_grande.png?v=1480481052',
+  'https://www.dictionary.com/e/wp-content/uploads/2018/03/Upside-Down_Face_Emoji.png',
+];
+
+```
+
+## 📌 Props
+
+-----
+Prop                  | Type     | Default                   | Required | Description
+--------------------- | -------- | ------------------------- | -------- | -----------
+radius|number|60|No|The radius of the circle the rendered items will render around.
+data|arrayOf[object Object]|[]|No|The data source to render.
+renderItem|func|({ item, index }) => null|No|A function to render an item from `data`.
+index|number|0|No|The currently focused index.
+onIndexChanged|func|(index) => null|No|Callback for when the index has changed.
+duration|number|300|No|The animation duration when rendered items are realigned.
+boost|number|3|No|The amount to scale focused items, compared to its nearest siblings.
+
+## ✌️ License
+[MIT]()
---
+++ index.js
@@ -0,0 +1,199 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { Platform, Animated, PanResponder } from 'react-native';
+import AnimatedMath from 'react-native-animated-math';
+
+const getIndex = (animRotate, data) => {
+  const dir = Math.sign(animRotate.__getValue());
+  const pct = animRotate.__getValue() / (2 * Math.PI);
+  const i = Math.round(data.length * pct) % data.length;
+  const j = data.length - i;
+  const k = j >= data.length ? 0 : j;
+  const l = dir * i;
+  return dir >= 0 ? k : l;
+};
+
+const shouldRest = (animRotate, data, duration) => {
+  animRotate.flattenOffset();
+  const d = Math.PI * 2 / data.length;
+  Animated
+    .timing(
+      animRotate,
+      {
+        toValue:  Math.round(animRotate.__getValue() / d) * d,
+        duration,
+        useNativeDriver: Platform.OS !== 'web',
+      },
+    )
+    .start(
+      () => animRotate.extractOffset(),
+    );
+};
+
+const Rotary = ({ radius, data, renderItem, index, onIndexChanged, duration, boost, ...extraProps }) => {
+  const [ animRotate ] = useState(
+    new Animated.Value(
+      -1 * index * Math.PI * 2 / data.length,
+    ),
+  );
+  const [ panResponder ] = useState(
+    PanResponder
+      .create(
+        {
+          onStartShouldSetPanResponder: () => false,
+          onMoveShouldSetPanResponder: () => true,
+          onPanResponderTerminationRequest: () => false,
+          onShouldBlockNativeResponder: () => true,
+          onPanResponderGrant: (evt, gestureState) => {
+            animRotate.extractOffset();
+          },
+          onPanResponderMove: (evt, gestureState) => {
+            const { dx } = gestureState;
+            const v = (dx / radius);
+            animRotate.setValue(
+              v,
+            );
+          },
+          onPanResponderRelease: (evt, gestureState) => {
+            onIndexChanged(getIndex(animRotate, data));
+            shouldRest(animRotate, data, duration);
+          },
+          onPanResponderTerminate: (evt, gestureState) => {
+            onIndexChanged(getIndex(animRotate, data));
+            shouldRest(animRotate, data, duration);
+          },
+        },
+      )
+  );
+  useEffect(
+    () => {
+      const curr = getIndex(animRotate, data);
+      if (index !== curr) {
+        animRotate.flattenOffset();
+        Animated
+          .timing(
+            animRotate,
+            {
+              toValue: -1 * index * Math.PI * 2 / data.length,
+              duration,
+              useNativeDriver: Platform.OS !== 'web',
+            },
+          )
+          .start();
+      }
+    },
+  );
+  return (
+    <Animated.View
+      {...panResponder.panHandlers}
+      style={{
+        width: radius * 2,
+        height: radius * 1.25,
+        overflow: 'hidden',
+      }}
+    >
+      {data.map(
+        (item, index, { length }) => {
+          const offsetRotate = Animated
+            .add(
+              animRotate,
+              Math.PI,
+            );
+          const scale = AnimatedMath.pow(
+            AnimatedMath
+              .sinus(
+                Animated.diffClamp(
+                  Animated.modulo(
+                    Animated.add(
+                      Animated.add(
+                        offsetRotate,
+                        -1 * Math.PI / 2,
+                      ),
+                      index / length * 2 * Math.PI,
+                    ),
+                    Math.PI * 2,
+                  ),
+                  -radius,
+                  radius,
+                ),
+              ),
+            boost,
+          );
+          return (
+            <Animated.View
+              key={index}
+              style={{
+                opacity: scale,
+                position: 'absolute',
+                transform: [
+                  { translateX: Animated
+                      .add(
+                        Animated.multiply(
+                          radius,
+                          AnimatedMath
+                            .cosinus(
+                              Animated
+                                .add(
+                                  (index / length) * Math.PI * 2 + Math.PI / 2,
+                                  offsetRotate,
+                                ),
+                            ),
+                        ),
+                        radius * 0.75,
+                      )},
+                  { translateY: Animated
+                      .add(
+                        Animated.multiply(
+                          radius,
+                          AnimatedMath
+                            .sinus(
+                              Animated
+                                .add(
+                                  (index / length) * Math.PI * 2 + Math.PI / 2,
+                                  offsetRotate,
+                                ),
+                            ),
+                        ),
+                        radius * 0.75 * 1.65,
+                      )},
+                  { scale: Animated
+                    .multiply(
+                      scale,
+                      2,
+                    )
+                  },
+                ],
+              }}
+            >
+              {renderItem({ item, index })}
+            </Animated.View>
+          );
+        }
+      )}
+    </Animated.View>
+  );
+};
+
+Rotary.propTypes = {
+  radius: PropTypes.number,
+  data: PropTypes.arrayOf(
+    PropTypes.any,
+  ),
+  renderItem: PropTypes.func,
+  index: PropTypes.number,
+  onIndexChanged: PropTypes.func,
+  duration: PropTypes.number,
+  boost: PropTypes.number,
+};
+
+Rotary.defaultProps = {
+  radius: 60,
+  data: [],
+  renderItem: ({ item, index }) => null,
+  index: 0,
+  onIndexChanged: (index) => null,
+  duration: 300,
+  boost: 3,
+};
+
+export default Rotary;
